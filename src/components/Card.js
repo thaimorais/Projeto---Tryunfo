df213cedd5f5945615d09d0420e157fdbc58db46
Thaísy Morais <102544688+thaimorais@users.noreply.github.com>
2022-09-02 18:51:24 -0300

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,7 +13,12 @@ class Card extends React.Component {
     return (
       <div className="card-preview">
         <p data-testid="name-card">{ cardName }</p>
-        <img data-testid="image-card" className="image-card" src={ cardImage } alt={ cardName } />
+        <img
+          data-testid="image-card"
+          className="image-card"
+          src={ cardImage }
+          alt={ cardName }
+        />
         <p data-testid="description-card">{ cardDescription }</p>
         <p data-testid="attr1-card">{ cardAttr1 }</p>
         <p data-testid="attr2-card">{ cardAttr2 }</p>
